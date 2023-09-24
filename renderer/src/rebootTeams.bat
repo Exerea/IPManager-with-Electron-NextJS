bad480fe2ebc9620990568306324c2618d8f7b99
Exerea <ryu.vs.lime.and.takumi@ezweb.ne.jp>
2023-09-24 23:56:45 +0900

    fix softwaredata

--- renderer/src/rebootTeams.bat
+++ renderer/src/rebootTeams.bat
@@ -1,7 +1,5 @@
 @echo off
-@REM taskkill /IM Teams.exe /F
-taskkill /IM steam.exe /F
+taskkill /IM Teams.exe /F
 timeout /t 5
-@REM start "" "C:\Program Files (x86)\Microsoft\Teams\current\Teams.exe"
-start "" "C:\Program Files (x86)\Steam\steam.exe"
+start "" "C:\Program Files (x86)\Microsoft\Teams\current\Teams.exe"
 exit
